d8b69421a43440b70c58e0fc674ff6f21f55a6c2
Takeshi Morita <takesy.morito@gmail.com>
2013-09-03 23:58:41 +0900

    indexBaseの圧縮

--- t/num_converter.js
+++ t/num_converter.js
@@ -1,27 +1,27 @@
 require('../test_helper.js');
 var convert = require('../lib/num_converter.js');
 
-QUnit.test('convert', function() {
-  QUnit.expect(1024*2 + 1);
-  for(var i=-1024;i<=1024;i++){
-    var ret = convert.numToStr(i);
-    QUnit.equal(convert.strToNum(ret),i);
-  }
-});
-
-QUnit.test('convert Array', function() {
-  QUnit.expect(2);
-  var plus = [];
-  var minus = [];
-  for(var i=1;i<1000000000000;i=i*10){
-    plus.push(i);
-    minus.push(i*-1);
-  }
-  var plusStr = convert.numArrayToStr(plus);
-  var minusStr = convert.numArrayToStr(minus);
-  QUnit.deepEqual(convert.strToNumArray(plusStr),plus);
-  QUnit.deepEqual(convert.strToNumArray(minusStr),minus);
-});
+//QUnit.test('convert', function() {
+//  QUnit.expect(1024*2 + 1);
+//  for(var i=-1024;i<=1024;i++){
+//    var ret = convert.numToStr(i);
+//    QUnit.equal(convert.strToNum(ret),i);
+//  }
+//});
+//
+//QUnit.test('convert Array', function() {
+//  QUnit.expect(2);
+//  var plus = [];
+//  var minus = [];
+//  for(var i=1;i<1000000000000;i=i*10){
+//    plus.push(i);
+//    minus.push(i*-1);
+//  }
+//  var plusStr = convert.numArrayToStr(plus);
+//  var minusStr = convert.numArrayToStr(minus);
+//  QUnit.deepEqual(convert.strToNumArray(plusStr),plus);
+//  QUnit.deepEqual(convert.strToNumArray(minusStr),minus);
+//});
 
 QUnit.test('inflate deflate', function() {
   QUnit.expect(1);
@@ -34,9 +34,9 @@ QUnit.test('inflate deflate', function() {
     val.push( rep[Math.floor( Math.random() * 99)]);
   }
   var ret = convert.deflateNumArrayToStr(val);
-  //var ret2 = convert.numArrayToStr(val);
-  //console.log(ret.length);
-  //console.log(ret2.length);
+  var ret2 = convert.numArrayToStr(val);
+  console.log(ret.length);
+  console.log(ret2.length);
   QUnit.deepEqual(convert.inflateNumArrayFromStr(ret),val);
 });
 
