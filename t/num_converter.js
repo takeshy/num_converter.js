f5e06531d11b7790f95c0bf22be05b29e60b2e22
Takeshi Morita <morita@minkabu.co.jp>
2013-09-03 17:48:26 +0900

    inflate,deflate対応

--- t/num_converter.js
+++ t/num_converter.js
@@ -1,13 +1,13 @@
 require('../test_helper.js');
 var convert = require('../lib/num_converter.js');
 
-//QUnit.test('convert', function() {
-//  QUnit.expect(65536*2 + 1);
-//  for(var i=-65536;i<=65536;i++){
-//    var ret = convert.numToStr(i);
-//    QUnit.equal(convert.strToNum(ret),i);
-//  }
-//});
+QUnit.test('convert', function() {
+  QUnit.expect(1024*2 + 1);
+  for(var i=-1024;i<=1024;i++){
+    var ret = convert.numToStr(i);
+    QUnit.equal(convert.strToNum(ret),i);
+  }
+});
 
 QUnit.test('convert Array', function() {
   QUnit.expect(2);
@@ -21,6 +21,20 @@ QUnit.test('convert Array', function() {
   var minusStr = convert.numArrayToStr(minus);
   QUnit.deepEqual(convert.strToNumArray(plusStr),plus);
   QUnit.deepEqual(convert.strToNumArray(minusStr),minus);
+});
+
+QUnit.test('inflate deflate', function() {
+  QUnit.expect(1);
+  var val = [];
+  var rep = []
+  for(var i=0;i<=200;i++){
+    rep.push(Math.floor( Math.random() * 1000));
+  }
+  for(var i=0;i<=400;i++){
+    val.push( rep[Math.floor( Math.random() * 199)]);
+  }
+  var ret = convert.deflateNumArrayToStr(val);
+  QUnit.deepEqual(convert.inflateNumArrayFromStr(ret),val);
 });
 
 QUnit.start();
