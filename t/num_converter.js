e649da43277cc8ef00c5249d9baf64d6a3d36a87
Takeshi Morita <takesy.morito@gmail.com>
2013-09-04 22:41:19 +0900

    -32の差が32回続いた場合の対応

--- t/num_converter.js
+++ t/num_converter.js
@@ -1,27 +1,27 @@
 require('../test_helper.js');
 var convert = require('../lib/num_converter.js');
 
-//QUnit.test('convert', function() {
-//  QUnit.expect(1024*2 + 1);
-//  for(var i=-1024;i<=1024;i++){
-//    var ret = convert.numToStr(i);
-//    QUnit.equal(convert.strToNum(ret),i);
-//  }
-//});
-//
-//QUnit.test('convert Array', function() {
-//  QUnit.expect(2);
-//  var plus = [];
-//  var minus = [];
-//  for(var i=1;i<1000000000000;i=i*10){
-//    plus.push(i);
-//    minus.push(i*-1);
-//  }
-//  var plusStr = convert.numArrayToStr(plus);
-//  var minusStr = convert.numArrayToStr(minus);
-//  QUnit.deepEqual(convert.strToNumArray(plusStr),plus);
-//  QUnit.deepEqual(convert.strToNumArray(minusStr),minus);
-//});
+QUnit.test('convert', function() {
+  QUnit.expect(1024*2 + 1);
+  for(var i=-1024;i<=1024;i++){
+    var ret = convert.numToStr(i);
+    QUnit.equal(convert.strToNum(ret),i);
+  }
+});
+
+QUnit.test('convert Array', function() {
+  QUnit.expect(2);
+  var plus = [];
+  var minus = [];
+  for(var i=1;i<1000000000000;i=i*10){
+    plus.push(i);
+    minus.push(i*-1);
+  }
+  var plusStr = convert.numArrayToStr(plus);
+  var minusStr = convert.numArrayToStr(minus);
+  QUnit.deepEqual(convert.strToNumArray(plusStr),plus);
+  QUnit.deepEqual(convert.strToNumArray(minusStr),minus);
+});
 
 QUnit.test('continuous', function() {
   QUnit.expect(1);
@@ -36,9 +36,8 @@ QUnit.test('continuous', function() {
 QUnit.test('repeat chara', function() {
   QUnit.expect(1);
   var val = [];
-  var rep = [32,0,-32];
-  for(var i=0;i<=1000;i++){
-    val.push( rep[Math.floor( Math.random() * 3)]);
+  for(var i=0;i<=33;i++){
+    val.push( -32 * i);
   }
   var ret = convert.numArrayToStr(val);
   QUnit.deepEqual(convert.strToNumArray(ret),val);
